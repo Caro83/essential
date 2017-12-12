4c80d695de2bff03d4cc133f6172cde56b5cc046
Carole <leroux.carole@gmail.com>
2017-12-12 16:38:31 +0100

    Correction jquery navbar couleur

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+/* Menu hamburger */
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -8,6 +9,41 @@ function myFunction() {
 }
 
 $(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll < 300) {
+      $(".topnav").css("background" , "transparent");
+      $(".topnav").css("color", "#fff");
+      $(".topnav a").css("color", "#fff");
+    }
+
+    else{
+      $(".topnav").css("background" , "#fff");
+      $(".topnav").css("color", "#000");
+      $(".topnav a").css("color", "#000");
+    }
+  })
+})
+
+/* Change la couleur de la navbar en scrollant */
+/*$(window).scroll(function() {
+  if ($(document).scrollTop() > 200) {
+    $('.topnav').addClass('color-change');
+  } else {
+    $('.topnav').removeClass('color-change');
+  }
+});
+
+/* BOuton scroll down */
+$(function() {
+  $('#section05').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $('section.about').offset().top}, 500, 'linear');
+  });
+});
+
+/* Carrousel */
+$(document).ready(function(){
   $('.testimonials-carousel').slick({
     slidesToShow:1,
   	slidesToScroll: 1,
@@ -16,17 +52,4 @@ $(document).ready(function(){
   });
 });
 
-$(function() {
-  $('#section05').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $('section.about').offset().top}, 500, 'linear');
-  });
-});
 
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 200) {
-    $('.topnav').addClass('color-change');
-  } else {
-    $('.topnav').removeClass('color-change');
-  }
-});
